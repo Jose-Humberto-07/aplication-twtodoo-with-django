b24e17a38fbf4ab4a9e79614b7170cf3a6e360fd
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2024-01-31 21:57:03 -0300

    function create toodo

--- todos/views.py
+++ todos/views.py
@@ -1,12 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from django.views.generic import ListView, CreateView
+
+from django.urls import reverse_lazy
+
 from .models import Todo
 # Create your views here.
 
 
-def todo_list(request):
-    todos = Todo.objects.all()
+#def todo_list(request):
+    #todos = Todo.objects.all()
 
-    nomes = ["neymar, messi, cristiano ronaldo"]
-    return render(request, "todos/todo_list.html", {"todos": todos})
+   #nomes = ["neymar, messi, cristiano ronaldo"]
+    #return render(request, "todos/todo_list.html", {"todos": todos})
+
+
+
+class TodoListView(ListView):
+
+    model = Todo
+
+
+class TodoCreateView(CreateView):
+
+     model = Todo
+     fields = ["title", "deadline"]
+     success_url = reverse_lazy("todo_list")
+

26492d810428666a2fe443669f99fc86567074db
Jose-Humberto-07 <jhpnascimento96@gmail.com>
2024-02-01 22:29:15 -0300

    add function update

--- todos/views.py
+++ todos/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from django.views.generic import ListView, CreateView
+from django.views.generic import ListView, UpdateView, CreateView
 
 from django.urls import reverse_lazy
 
@@ -23,6 +23,12 @@ class TodoListView(ListView):
 
 
 class TodoCreateView(CreateView):
+
+     model = Todo
+     fields = ["title", "deadline"]
+     success_url = reverse_lazy("todo_list")
+
+class TodoUpdateView(UpdateView):
 
      model = Todo
      fields = ["title", "deadline"]
